da7aec7be748d52af1f972e180b76e57c4232468
Jacob Steirer <jxs1773@miami.edu>
2022-04-18 22:01:25 -0400

    Initial Commit

---
+++ City Sights App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City Sights App
+//
+//  Created by Jake Steirer on 4/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
